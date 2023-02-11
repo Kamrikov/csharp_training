a64ca3b9185847f9aebf3313e845a84918d05d0b
kamrikov-o <kamrikov.qa@gmail.com>
2023-02-11 13:55:55 +0300

    После Лекции 2.5

--- addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
@@ -26,11 +26,22 @@ namespace WebAddressbookTests
             RetornToGroupsPage();
             return this;
         }
+
+        public GroupHelper Modify(int p, GroupData newData)
+        {
+            manager.Navigator.GoToGroupsPage();
+            SelectGroup(p);
+            InitGroupModification();
+            FillGroupForm(newData);
+            SubmitGroupModification();
+            RetornToGroupsPage();
+            return this;
+        }
         public GroupHelper Remove(int p)
         {
             manager.Navigator.GoToGroupsPage();
 
-            SelectGroup(1);
+            SelectGroup(p);
             RemoveGroup();
             RetornToGroupsPage();
             return this;
@@ -71,6 +82,17 @@ namespace WebAddressbookTests
         public GroupHelper RemoveGroup()
         {
             driver.FindElement(By.Name("delete")).Click();
+            return this;
+        }
+        public GroupHelper SubmitGroupModification()
+        {
+            driver.FindElement(By.Name("update")).Click();
+            return this;
+        }
+
+        public GroupHelper InitGroupModification()
+        {
+            driver.FindElement(By.Name("edit")).Click();
             return this;
         }
     }
---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupModificationTests : TestBase
+    {
+        [Test]
+        public void GroupModificationTest()
+        {
+            GroupData newData = new GroupData("тест3");
+            newData.Header = "тест3";
+            newData.Footer = "тест3";
+
+            app.Groups.Modify(1, newData);
+        }
+    }
+}

364713493a8ddfb636389d5a755894f37c4c333d
kamrikov-o <kamrikov.qa@gmail.com>
2023-03-03 19:27:17 +0300

    После Лекции 5.3

--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -8,6 +8,8 @@ namespace WebAddressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
+        private string allPhones;
+
         public ContactData(string firstName, string lastName)
         {
             FirstName = firstName;
@@ -48,12 +50,55 @@ namespace WebAddressbookTests
             {
                 return FirstName.CompareTo(other.FirstName);
             }
-            
 
             // return LastName.CompareTo(other.LastName) + FirstName.CompareTo(other.FirstName);
         }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Id { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+       /* public string AllEmails 
+        { 
+            get 
+            { 
+
+            } 
+            set 
+            {
+
+            } 
+        } */
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+            set
+            {
+                allPhones = value;
+            }
+        }
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == " ")
+            {
+                return "";
+            }
+            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
     }
 }
---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactInformationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactInformationTests : AuthTestBase
+    {
+        [Test]
+        public void TestContactInformation()
+        {
+            ContactData fromTable = app.Contacts.GetContactInformationFromTable(0);
+            ContactData fromForm = app.Contacts.GetContactInformationFromEditForm(0);
+
+            // проверка
+            Assert.AreEqual(fromTable, fromForm);
+            Assert.AreEqual(fromTable.Address, fromForm.Address);
+            //Assert.AreEqual(fromTable.AllEmails, fromForm.AllEmails);
+            Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
+        }
+    }
+}

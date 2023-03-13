785e0fc1bda725967c4e91d31f9db86d1d803cc4
kamrikov-o <kamrikov.qa@gmail.com>
2023-03-13 11:08:34 +0300

    После Лекции 6.4

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
 using WebAddressbookTests;
 
 namespace addressbook_test_data_generators
@@ -16,8 +17,8 @@ namespace addressbook_test_data_generators
         static void Main(string[] args)
         {
             int count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
-            string format = args[2];
+            string fileName = args[1];
+            string format = args[2];    
 
             List<GroupData> groups = new List<GroupData>();
             for (int i = 0; i < count; i++)
@@ -28,25 +29,56 @@ namespace addressbook_test_data_generators
                     Footer = TestBase.GenerateRandomString(100)
                 });
             }
-            if (format == "csv")
+            if (format == "excel")
             {
-                writeGroupsToCsvFile(groups, writer);
-            }
-            else if (format == "xml")
-            {
-                writeGroupsToXmlFile(groups, writer);
-            }
-            else if (format == "json")
-            {
-                writeGroupsToJsonFile(groups, writer);
-            }
+                WriteGroupsToExcelFile(groups, fileName);
+            } 
             else
             {
-                System.Console.Out.Write("Unrecognized format" + format);
+                StreamWriter writer = new StreamWriter(fileName);
+                if (format == "csv")
+                {
+                    WriteGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer);
+                }
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format" + format);
+                }
+                writer.Close();
             }
-            writer.Close();
         }
-        static void writeGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        static void WriteGroupsToExcelFile(List<GroupData> groups, string fileName)
+        {
+            Excel.Application app = new Excel.Application();
+            app.Visible = true;
+            Excel.Workbook wb = app.Workbooks.Add();
+            Excel.Worksheet sheet = wb.ActiveSheet;
+
+            int row = 1;
+            foreach (GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+                row++;
+            }
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath);
+            wb.Close();
+            app.Visible = false;
+            app.Quit();
+        }
+
+        static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
         {
             foreach (GroupData group in groups)
             {
@@ -54,11 +86,11 @@ namespace addressbook_test_data_generators
                     group.Name, group.Header, group.Footer));
             }
         }
-        static void writeGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
+        static void WriteGroupsToXmlFile(List<GroupData> groups, StreamWriter writer)
         {
             new XmlSerializer(typeof(List<GroupData>)).Serialize(writer, groups);
         }
-        static void writeGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
+        static void WriteGroupsToJsonFile(List<GroupData> groups, StreamWriter writer)
         {
             writer.Write(JsonConvert.SerializeObject(groups, Newtonsoft.Json.Formatting.Indented));
         }

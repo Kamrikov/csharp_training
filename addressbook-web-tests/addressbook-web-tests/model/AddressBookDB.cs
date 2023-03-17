91367743d6768352abbd0d196c977ceb0f7dd19e
kamrikov-o <kamrikov.qa@gmail.com>
2023-03-17 18:27:31 +0300

    После Лекции 7.1 и ответа на вопросы

--- addressbook-web-tests/addressbook-web-tests/model/AddressBookDB.cs
+++ addressbook-web-tests/addressbook-web-tests/model/AddressBookDB.cs
@@ -10,7 +10,7 @@ namespace WebAddressbookTests
     public class AddressBookDB : LinqToDB.Data.DataConnection
     {
         public AddressBookDB() : base("AddressBook") { }
-        public ITable<GroupData> Groups { get { return GetTable<GroupData>(); } }
-        public ITable<ContactData> Contacts { get { return GetTable<ContactData>(); } }
+        public ITable<GroupData> Groups { get { return this.GetTable<GroupData>(); } }
+        public ITable<ContactData> Contacts { get { return this.GetTable<ContactData>(); } }
     }
 }
